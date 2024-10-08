6b01ac58dd55b65bfb265afaa7fe076fa52867e3
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-08 18:30:46 -0300

    Refactor: Remove unnecessary div/section

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,7 +1,7 @@
 import { parent, createDivOrSection, createHeading, createP, createBtn, createForm, createInput, createSmall } from "../functions/createElement.js";
 
 function trialBtn(){
-    createBtn(parent(".container"), "container__free__trial-button")
+    createBtn(parent(".container"), "container__free__trial-button", "#")
     createP(parent(".container__free__trial-button"), "free__trial__content", "")
     const freeTrialBtnContent = document.querySelector(".free__trial__content")
     freeTrialBtnContent.innerHTML = `
@@ -10,10 +10,10 @@ function trialBtn(){
 }
 
 function initInput(parentElement){
-    createInput(parent(parentElement), "input__fname", "text", "fname", "fname", "First name")
-    createInput(parent(parentElement), "input__lname", "text", "lname", "lname", "Last name")
-    createInput(parent(parentElement), "input__email", "email", "email", "email", "Email")
-    createInput(parent(parentElement), "input__password", "password", "password", "password", "Password")
+    createInput(parent(parentElement), "input", "text", "fname", "fname", "First name")
+    createInput(parent(parentElement), "input", "text", "lname", "lname", "Last name")
+    createInput(parent(parentElement), "input", "email", "email", "email", "Email")
+    createInput(parent(parentElement), "input", "password", "password", "password", "Password")
 }
 
 function initHtmlBody(){
@@ -24,14 +24,12 @@ function initHtmlBody(){
     createDivOrSection(document.body, "SECTION", "container")
     trialBtn()
 
-    createDivOrSection(parent(".container"), "DIV", "container__div")
-    createForm(parent(".container__div"), "container__div__form")
+    createForm(parent(".container"), "container__div__form")
     initInput(".container__div__form")
 
-    createBtn(parent(".container__div"), "container__div__btn")
+    createBtn(parent(".container__div__form"), "container__div__btn", "#")
     createP(parent(".container__div__btn"), "container__div__btn-content", "Claim your free trial")
-    createDivOrSection(parent(".container__div"), "DIV","container__div__condition")
-    createSmall(parent(".container__div__condition"), "container__div__condition-small", "")
+    createSmall(parent(".container__div__form"), "container__div__condition-small", "")
     const smallText = document.querySelector(".container__div__condition-small")
     smallText.innerHTML = `
     By clicking the button, you are agreeing to our <span>Terms and Services</span>
